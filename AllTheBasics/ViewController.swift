d13d04cc13bbaeddd093e06b5d58b322d57c9722
Bejan Fozdar <ixyzt@users.noreply.github.com>
2016-09-01 07:42:05 -0700

    Complete

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -15,7 +15,32 @@ class ViewController: UIViewController {
     }
     
     
-   // Implement your functions here
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        if (a + b + c) / 3 > 75.0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    func passwordCombo(user: String, password: Int) -> String {
+        let approvedUser = ["Jerry","Elaine","Michael"]
+        if approvedUser.contains(user) && password % 3 == 0 {
+            return "Welcome!"
+        } else {
+            return "Access Denied"
+        }
+    }
+    
+    func numberGenerator(a: String, b: Float) -> Float {
+        let stringToFloat = Float(a)
+        if (Float(1) < stringToFloat && Float(5) >= stringToFloat) && ((Float(10.5) < b && Float(15.0) >= b) || (Float(20.0) < b && Float(30.5) >= b)) {
+            return Float(stringToFloat!*b)
+        } else {
+            return Float(0)
+        }
+    }
 
     
 
